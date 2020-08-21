c4cf66d506698ff94f72dcf822124ce69f9fb3f9
sjol <samuel.jollois@cgi.com>
2020-08-21 09:26:18 +0200

    fix css + override theme + bugs

--- src/main/resources/public/ts/behaviours.ts
+++ src/main/resources/public/ts/behaviours.ts
@@ -6,7 +6,7 @@ Behaviours.register('crre', {
             access: 'fr.openent.crre.controllers.CrreController|view',
             administrator: 'fr.openent.crre.controllers.AgentController|createAgent',
             manager: 'fr.openent.crre.controllers.AgentController|getAgents',
-            prescriptor: 'fr.openent.crre.controller.AgentController|deleteAgent',
+            prescriptor: 'fr.openent.crre.controllers.AgentController|deleteAgent',
             validator: 'fr.openent.crre.controllers.AgentController|updateAgent'
         },
         resource: {}
