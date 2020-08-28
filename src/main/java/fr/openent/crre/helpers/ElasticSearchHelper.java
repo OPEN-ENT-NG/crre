8c658033970c04989518de59198fc935e100fd6a
BERNOUSSI-HAZIM <sofiane.bernoussi-hazim@cgi.com>
2020-08-28 15:52:38 +0200

    Add ElasticSearchHelper

---
+++ src/main/java/fr/openent/crre/helpers/ElasticSearchHelper.java
@@ -0,0 +1,95 @@
+package fr.openent.crre.helpers;
+
+
+import fr.wseduc.webutils.Either;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.entcore.common.elasticsearch.ElasticSearch;
+
+import java.util.*;
+
+public class ElasticSearchHelper {
+    private static final String REGEXP_FORMAT = ".*%s.*";
+    private static final Integer PAGE_SIZE = 10000;
+    private static final String RESOURCE_TYPE_NAME = "equipment";
+    private static final  List<String> PLAIN_TEXT_FIELDS = Arrays.asList("id", "name", "ean");
+
+    private ElasticSearchHelper() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static String formatRegexp(String query) {
+        return String.format(REGEXP_FORMAT, query.toLowerCase());
+    }
+
+    private static void search(JsonObject query, Handler<Either<String, JsonArray>> handler) {
+        executeEsSearch(query, ar -> {
+            if (ar.failed()) {
+                handler.handle(new Either.Left<>(ar.cause().toString()));
+            } else {
+                JsonArray result = new JsonArray();
+                for (Object article:ar.result()) {
+                    result.add(((JsonObject)article).getJsonObject("_source"));
+                }
+                handler.handle(new Either.Right<>(result));
+            }
+        });
+    }
+
+
+    public static void plainTextSearch(String query, Handler<Either<String, JsonArray>> handler) {
+        JsonArray should = new JsonArray();
+        for (String field : PLAIN_TEXT_FIELDS) {
+            JsonObject regexp = new JsonObject()
+                    .put(field, formatRegexp(query));
+            should.add(new JsonObject().put("regexp", regexp));
+        }
+
+        JsonObject regexpBool = new JsonObject()
+                .put("should", should);
+        JsonArray must = new JsonArray()
+        .add(new JsonObject().put("bool", regexpBool));
+
+        JsonObject bool = new JsonObject()
+                .put("must", must);
+        JsonObject queryObject = new JsonObject()
+                .put("bool", bool);
+
+        search(esQueryObject(queryObject), handler);
+    }
+
+    private static void executeEsSearch(JsonObject query, Handler<AsyncResult<JsonArray>> handler) {
+        ElasticSearch.getInstance().search(RESOURCE_TYPE_NAME, query, search -> {
+            if (search.failed()) {
+                handler.handle(Future.failedFuture(search.cause()));
+            } else {
+                List<JsonObject> resources = parseEsResponse(search.result());
+                handler.handle(Future.succeededFuture(new JsonArray(resources)));
+
+            }
+        });
+    }
+
+    private static List<JsonObject> parseEsResponse(JsonObject esResponse) {
+        return esResponse
+                .getJsonObject("hits", new JsonObject())
+                .getJsonArray("hits", new JsonArray()).getList();
+    }
+
+    private static JsonObject esQueryObject(JsonObject query) {
+        return new JsonObject()
+                .put("query", query)
+                .put("from", 0)
+                .put("size", PAGE_SIZE);
+    }
+
+    private static JsonObject regexpField(String field, String query) {
+        JsonObject regexp = new JsonObject()
+                .put(field, formatRegexp(query));
+
+        return new JsonObject().put("regexp", regexp);
+    }
+}
