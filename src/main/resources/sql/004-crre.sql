07b7dd8bbbda6f3e2bd91ad7bf7d083fbc4bb0df
sber <sofiane.bernoussi-hazim@cgi.com>
2021-01-25 10:38:19 +0100

    Fix script sql

--- src/main/resources/sql/004-crre.sql
+++ src/main/resources/sql/004-crre.sql
@@ -4,5 +4,5 @@ ALTER TABLE crre.basket_equipment
 ALTER TABLE crre.order_client_equipment
     ADD COLUMN reassort boolean NOT NULL DEFAULT false;
 
-ALTER TABLE crre.order_region_equipment
+ALTER TABLE crre."order-region-equipment"
     ADD COLUMN reassort boolean NOT NULL DEFAULT false;
