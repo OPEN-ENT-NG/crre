e0e0ed5ff92984e88c5bddaec069c5acf9859beb
sber <sofiane.bernoussi-hazim@cgi.com>
2021-02-15 10:31:27 +0100

    Fix ark search

--- src/main/java/fr/openent/crre/helpers/ElasticSearchHelper.java
+++ src/main/java/fr/openent/crre/helpers/ElasticSearchHelper.java
@@ -16,7 +16,7 @@ public class ElasticSearchHelper {
     private static final String REGEXP_FORMAT = ".*%s.*";
     private static final Integer PAGE_SIZE = 10000;
     private static final String RESOURCE_TYPE_NAME = "_doc";
-    private static final  List<String> PLAIN_TEXT_FIELDS = Arrays.asList("ean", "titre", "editeur", "disciplines", "niveaux", "auteur");
+    private static final  List<String> PLAIN_TEXT_FIELDS = Arrays.asList("ean", "titre", "editeur", "disciplines", "niveaux", "auteur", "ark");
 
     private ElasticSearchHelper() {
         throw new IllegalStateException("Utility class");
