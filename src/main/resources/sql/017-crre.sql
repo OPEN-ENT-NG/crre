1236e03a8ea278c4fe50fb9625cff760d3644c2d
sber <sofiane.bernoussi-hazim@cgi.com>
2021-04-09 11:06:39 +0200

    [CRRE-70] Decrochage ES fix

--- src/main/resources/sql/017-crre.sql
+++ src/main/resources/sql/017-crre.sql
@@ -1,3 +1,37 @@
+CREATE TABLE crre.order_client_equipment_old (
+    id bigserial NOT NULL,
+    amount bigint NOT NULL,
+    creation_date date NOT NULL DEFAULT CURRENT_DATE,
+    id_campaign bigint NOT NULL,
+    id_structure character varying NOT NULL,
+    status character varying(50) NOT NULL,
+    equipment_key VARCHAR,
+    equipment_name VARCHAR,
+    equipment_image VARCHAR,
+    equipment_price numeric,
+    equipment_grade VARCHAR,
+    equipment_editor VARCHAR,
+    equipment_diffusor VARCHAR,
+    equipment_format VARCHAR,
+    equipment_tva5 double precision,
+    equipment_tva20 double precision,
+    equipment_priceht double precision,
+    cause_status character varying(300),
+    comment TEXT,
+    user_id character varying(100) NOT NULL,
+    id_basket bigint,
+    reassort boolean NOT NULL DEFAULT false,
+    offers json,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_campaign_id FOREIGN KEY (id_campaign)
+     REFERENCES crre.campaign (id) MATCH SIMPLE
+     ON UPDATE NO ACTION
+     ON DELETE NO ACTION,
+    CONSTRAINT "Check_amount_positive" CHECK (amount >= 0::numeric),
+    CONSTRAINT "status_values" CHECK (status IN ('WAITING', 'VALID','IN PROGRESS', 'WAITING_FOR_ACCEPTANCE', 'REJECTED', 'SENT', 'DONE') ),
+    CONSTRAINT fk_basket_order FOREIGN KEY (id_basket) REFERENCES crre.basket_order(id)
+    );
+
 CREATE TABLE crre."order-region-equipment-old"
 (
     id bigserial NOT NULL,
@@ -36,40 +70,6 @@ CREATE TABLE crre."order-region-equipment-old"
     CONSTRAINT "Check_amount_positive" CHECK (amount::numeric >= 0::numeric) NOT VALID,
     CONSTRAINT "status_values" CHECK (status IN ('WAITING', 'VALID','IN PROGRESS', 'WAITING_FOR_ACCEPTANCE', 'REJECTED', 'SENT', 'DONE') )
 );
-
-CREATE TABLE crre.order_client_equipment_old (
-    id bigserial NOT NULL,
-    amount bigint NOT NULL,
-    creation_date date NOT NULL DEFAULT CURRENT_DATE,
-    id_campaign bigint NOT NULL,
-    id_structure character varying NOT NULL,
-    status character varying(50) NOT NULL,
-    equipment_key VARCHAR,
-    equipment_name VARCHAR,
-    equipment_image VARCHAR,
-    equipment_price numeric,
-    equipment_grade VARCHAR,
-    equipment_editor VARCHAR,
-    equipment_diffusor VARCHAR,
-    equipment_format VARCHAR,
-    equipment_tva5 double precision,
-    equipment_tva20 double precision,
-    equipment_priceht double precision,
-    cause_status character varying(300),
-    comment TEXT,
-    user_id character varying(100) NOT NULL,
-    id_basket bigint,
-    reassort boolean NOT NULL DEFAULT false,
-    offers json,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_campaign_id FOREIGN KEY (id_campaign)
-     REFERENCES crre.campaign (id) MATCH SIMPLE
-     ON UPDATE NO ACTION
-     ON DELETE NO ACTION,
-    CONSTRAINT "Check_amount_positive" CHECK (amount >= 0::numeric),
-    CONSTRAINT "status_values" CHECK (status IN ('WAITING', 'VALID','IN PROGRESS', 'WAITING_FOR_ACCEPTANCE', 'REJECTED', 'SENT', 'DONE') ),
-    CONSTRAINT fk_basket_order FOREIGN KEY (id_basket) REFERENCES crre.basket_order(id)
-    );
 
 ALTER TABLE crre."order-region-equipment"
 DROP CONSTRAINT fk_order_client_id,
