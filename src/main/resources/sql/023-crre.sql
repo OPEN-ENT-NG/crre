52ae63519b4d04317faa798cbd871b1e0306206e
sber <sofiane.bernoussi-hazim@cgi.com>
2021-10-06 10:07:03 +0200

    fix(sql) : [CRRE-168] Modify table old for cause status

--- src/main/resources/sql/023-crre.sql
+++ src/main/resources/sql/023-crre.sql
@@ -3,3 +3,9 @@ ALTER COLUMN cause_status TYPE text;
 
 ALTER TABLE crre.order_client_equipment
 ALTER COLUMN cause_status TYPE text;
+
+ALTER TABLE crre."order-region-equipment-old"
+ALTER COLUMN cause_status TYPE text;
+
+ALTER TABLE crre.order_client_equipment_old
+ALTER COLUMN cause_status TYPE text;
