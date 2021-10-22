d2b29314d0272bd97ea7da73da1769b73cf22b9c
sber <sofiane.bernoussi-hazim@cgi.com>
2021-10-22 14:17:31 +0200

    fix(stats): Update stats manually

--- src/main/java/fr/openent/crre/controllers/StatisticsController.java
+++ src/main/java/fr/openent/crre/controllers/StatisticsController.java
@@ -1,6 +1,7 @@
 package fr.openent.crre.controllers;
 
 import fr.openent.crre.Crre;
+import fr.openent.crre.cron.statistics;
 import fr.openent.crre.security.AdministratorRight;
 import fr.openent.crre.service.StatisticsService;
 import fr.openent.crre.service.StructureService;
@@ -13,6 +14,7 @@ import fr.wseduc.security.SecuredAction;
 import fr.wseduc.webutils.http.BaseController;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
@@ -30,11 +32,12 @@ import static fr.openent.crre.helpers.FutureHelper.handlerJsonObject;
 public class StatisticsController extends BaseController {
     private final StatisticsService statisticsService;
     private final StructureService structureService;
+    private final statistics statCron;
 
     public StatisticsController() {
         this.statisticsService = new DefaultStatisticsService(Crre.crreSchema);
         this.structureService = new DefaultStructureService(Crre.crreSchema);
-
+        this.statCron = new statistics(Vertx.vertx());
     }
 
     @Get("region/statistics/structures")
@@ -157,125 +160,14 @@ public class StatisticsController extends BaseController {
     @SecuredAction(value = "", type = ActionType.RESOURCE)
     @ResourceFilter(AdministratorRight.class)
     public void exportMongoStat(final HttpServerRequest request) {
-    List<Future> futures = new ArrayList<>();
-        structureService.getAllStructuresDetail(event -> {
-        if (event.isRight()) {
-            JsonArray structures = event.right().getValue();
-            for (int i = 0; i < structures.size(); i++) {
-                Future<JsonObject> addStructureStatFuture = Future.future();
-                futures.add(addStructureStatFuture);
-                JsonObject structure = structures.getJsonObject(i);
-                // All compute with structure object
-                structure.put("date", LocalDate.now().toString());
-                List<Future> futuresStat = new ArrayList<>();
-                Future<JsonArray> licencesFuture = Future.future();
-                Future<JsonArray> freeLicenceFuture = Future.future();
-                Future<JsonArray> totalRessourcesFuture = Future.future();
-                Future<JsonArray> ordersByYearFuture = Future.future();
-                Future<JsonArray> ordersByCampaignFuture = Future.future();
-                Future<JsonArray> allRessourceFuture = Future.future();
-                Future<JsonArray> numeriqueRessourceFuture = Future.future();
-                Future<JsonArray> papierRessourceFuture = Future.future();
-                futuresStat.add(licencesFuture);
-                futuresStat.add(freeLicenceFuture);
-                futuresStat.add(ordersByCampaignFuture);
-                futuresStat.add(ordersByYearFuture);
-                futuresStat.add(totalRessourcesFuture);
-                futuresStat.add(numeriqueRessourceFuture);
-                futuresStat.add(papierRessourceFuture);
-                futuresStat.add(allRessourceFuture);
-
-                CompositeFuture.all(futuresStat).setHandler(event2 -> {
-                    if (event2.succeeded()) {
-                        JsonArray licences = licencesFuture.result();
-                        JsonArray free_total = freeLicenceFuture.result();
-                        JsonArray ressources_total = totalRessourcesFuture.result();
-                        JsonArray order_year = ordersByYearFuture.result();
-                        JsonArray order_campaign = ordersByCampaignFuture.result();
-                        JsonArray numeric_ressources = numeriqueRessourceFuture.result();
-                        JsonArray paper_ressources = papierRessourceFuture.result();
-                        JsonArray all_ressources = allRessourceFuture.result();
-
-
-                        if (ressources_total.size() > 0 || order_year.size() > 0 || licences.size() > 0) {
-                            JsonObject stats = new JsonObject();
-                            // Every 30/04, add a new line in licences mongo db with new year
-                            if (LocalDate.now().getMonthValue() == 5 && LocalDate.now().getDayOfMonth() == 1) {
-                                licences.add(licences.getJsonObject(0));
-                            }
-                            // Put a n+1 year if after 30/04
-                            if (LocalDate.now().getMonthValue() < 5) {
-                                licences.getJsonObject(licences.size() - 1).put("year", LocalDate.now().getYear() + "");
-                            } else {
-                                licences.getJsonObject(licences.size() - 1).put("year", LocalDate.now().getYear() + 1 + "");
-                            }
-                            double licencesPercentage = 0;
-                            if(licences.getJsonObject(0).getInteger("initial_amount") == 0) {
-                                licencesPercentage = 0;
-
-                            } else {
-                                licencesPercentage = (licences.getJsonObject(licences.size() - 1).getInteger("amount") / licences.getJsonObject(0).getInteger("initial_amount")) * 100;
-                            }
-                            licences.getJsonObject(licences.size() - 1).put("percentage", 100 - licencesPercentage);
-                            stats.put("licences", licences);
-                            stats.put("free_total", free_total);
-                            stats.put("ressources_total", ressources_total);
-                            stats.put("order_year", order_year);
-                            stats.put("order_campaign", order_campaign);
-                            stats.put("numeric_ressources", numeric_ressources);
-                            stats.put("paper_ressources", paper_ressources);
-                            stats.put("all_ressources", all_ressources);
-                            structure.put("stats", stats);
-                        }
-                        statisticsService.exportMongo(structure, handlerJsonObject(addStructureStatFuture));
-
-                    } else {
-                        log.info("Failed to retrieve statistics for a structure");
-                    }
-                });
-
-                // Licences
-
-                statisticsService.getLicences(structure.getString("id_structure"), handlerJsonArray(licencesFuture));
-
-                // Commandes
-
-                statisticsService.getStats("orders", structure.getString("id_structure"), handlerJsonArray(ordersByYearFuture));
-                statisticsService.getOrdersByCampaign(structure.getString("id_structure"), handlerJsonArray(ordersByCampaignFuture));
-
-                // Licences
-
-                statisticsService.getStats("free", structure.getString("id_structure"), handlerJsonArray(freeLicenceFuture));
-
-                // Finances
-
-                statisticsService.getStats("ressources", structure.getString("id_structure"), handlerJsonArray(totalRessourcesFuture));
-
-                // Ressources
-
-                statisticsService.getStats("articlenumerique", structure.getString("id_structure"), handlerJsonArray(numeriqueRessourceFuture));
-                statisticsService.getStats("articlepapier", structure.getString("id_structure"), handlerJsonArray(papierRessourceFuture));
-                statisticsService.getStats("all_ressources", structure.getString("id_structure"), handlerJsonArray(allRessourceFuture));
-
+        log.info("CRRE statistics started");
+        statCron.insertStatistics(event1 -> {
+            if (event1.isRight()) {
+                log.info("Update statistics launch successful");
+            } else {
+                log.info("Update statistics failed");
             }
-            CompositeFuture.all(futures).setHandler(event2 -> {
-                if (event2.succeeded()) {
-                    log.info("Insert structures in mongo successful");
-                    statisticsService.deleteStatsDay(event1 -> {
-                        if(event1.isRight()) {
-                            log.info("Remove old stats of the day succesful");
-                        } else {
-                            log.info("Remove old stats of the day failed");
-                        }
-                    });
-                } else {
-                    log.info("Failed to insert structures in mongo");
-                }
-            });
-        } else {
-            log.info("Failed to get structures detail");
-        }
-    });
+        });
 }
 
     @Get("region/orders/mongo")
