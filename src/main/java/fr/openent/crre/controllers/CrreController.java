dbb316d6f0536599aa491121701d160d7bfdc857
sber <sofiane.bernoussi-hazim@cgi.com>
2022-08-11 16:43:11 +0200

    feat(config): add api routes to see config

--- src/main/java/fr/openent/crre/controllers/CrreController.java
+++ src/main/java/fr/openent/crre/controllers/CrreController.java
@@ -3,11 +3,14 @@ package fr.openent.crre.controllers;
 import fr.openent.crre.Crre;
 import fr.wseduc.rs.ApiDoc;
 import fr.wseduc.rs.Get;
+import fr.wseduc.security.ActionType;
 import fr.wseduc.security.SecuredAction;
 import io.vertx.core.http.HttpServerRequest;
 import org.entcore.common.controller.ControllerHelper;
 import org.entcore.common.events.EventStore;
 import org.entcore.common.events.EventStoreFactory;
+import org.entcore.common.http.filter.ResourceFilter;
+import org.entcore.common.http.filter.SuperAdminFilter;
 
 import static fr.openent.crre.Crre.ACCESS_RIGHT;
 
@@ -21,6 +24,13 @@ public class CrreController extends ControllerHelper {
     public CrreController() {
         super();
         this.eventStore = EventStoreFactory.getFactory().getEventStore(Crre.class.getSimpleName());
+    }
+
+    @Get("/config")
+    @SecuredAction(value = "", type = ActionType.RESOURCE)
+    @ResourceFilter(SuperAdminFilter.class)
+    public void getConfig(final HttpServerRequest request) {
+        renderJson(request, config);
     }
 
     @Get("")
