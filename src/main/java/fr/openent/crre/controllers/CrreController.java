c9b0ebf6059501764e512b9f346bb1e2d8309dd4
Samuel JOLLOIS <sjollois@ensc.fr>
2022-08-25 09:13:34 +0200

    fix(config) : don't give username/password of ElasticSearch Server

--- src/main/java/fr/openent/crre/controllers/CrreController.java
+++ src/main/java/fr/openent/crre/controllers/CrreController.java
@@ -6,6 +6,7 @@ import fr.wseduc.rs.Get;
 import fr.wseduc.security.ActionType;
 import fr.wseduc.security.SecuredAction;
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.JsonObject;
 import org.entcore.common.controller.ControllerHelper;
 import org.entcore.common.events.EventStore;
 import org.entcore.common.events.EventStoreFactory;
@@ -30,8 +31,15 @@ public class CrreController extends ControllerHelper {
     @SecuredAction(value = "", type = ActionType.RESOURCE)
     @ResourceFilter(SuperAdminFilter.class)
     public void getConfig(final HttpServerRequest request) {
-        renderJson(request, config);
-    }
+        JsonObject safeConfig = config.copy();
+
+        JsonObject elasticsearchConfig = safeConfig.getJsonObject("elasticsearchConfig", null);
+        if (elasticsearchConfig != null) {
+            if (elasticsearchConfig.getString("username", null) != null) elasticsearchConfig.put("username", "**********");
+            if (elasticsearchConfig.getString("password", null) != null) elasticsearchConfig.put("password", "**********");
+        }
+
+        renderJson(request, safeConfig);    }
 
     @Get("")
     @ApiDoc("Display the home view")
