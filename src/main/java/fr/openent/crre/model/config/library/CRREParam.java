1b62b47625a29cf75f47295e0f8dd17a74d0415b
BastienBenard <95362455+BastienBenard@users.noreply.github.com>
2023-04-20 15:07:38 +0200

    feat(multi-library): #CRRE-574 creation of a library interface (#133)
    Co-authored-by: Benard Bastien <bastien.benard@cgi.com>

---
+++ src/main/java/fr/openent/crre/model/config/library/CRREParam.java
@@ -0,0 +1,41 @@
+package fr.openent.crre.model.config.library;
+
+import fr.openent.crre.helpers.IModelHelper;
+import io.vertx.core.json.JsonObject;
+import fr.openent.crre.core.constants.Field;
+
+public class CRREParam implements ILibraryConfigModel {
+    private String email;
+    private String apiUrl;
+
+    public CRREParam() {
+    }
+
+    public CRREParam(JsonObject jsonObject) {
+        this.email = jsonObject.getString(Field.EMAIL);
+        this.apiUrl = jsonObject.getString(Field.APIURL);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CRREParam setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public CRREParam setApiUrl(String apiUrl) {
+        this.apiUrl = apiUrl;
+        return this;
+    }
+
+    @Override
+    public JsonObject toJson() {
+        return IModelHelper.toJson(this, true, false);
+    }
+}
---
+++ src/main/java/fr/openent/crre/model/config/library/ILibraryConfigModel.java
@@ -0,0 +1,6 @@
+package fr.openent.crre.model.config.library;
+
+import fr.openent.crre.model.IModel;
+
+public interface ILibraryConfigModel extends IModel<ILibraryConfigModel> {
+}
